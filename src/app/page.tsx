ac26f701025c0e03d2b2f064e1faf735124d81ac
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-05-25 17:28:09 -0700

    Packages Reintate 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+  const handleDropdownItemClick = () => {
+    router.push('/auth/login');
+  };
   return (
-    <div>
-      <h1>CLICK ME</h1>
+    <div className="flex justify-center items-center h-screen" onClick={handleDropdownItemClick}>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <h1>CLICK ME</h1>
+      </button>
     </div>
+
   );
 }
