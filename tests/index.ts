c5e5ae233b3e3d8aa8b0b4e568d281af6f15725c
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-05-27 01:39:21 -0700

    json-scrapes of electives stored

--- tests/index.ts
+++ tests/index.ts
@@ -1,38 +1,80 @@
 
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-const PDFExtract = require('pdf.js-extract').PDFExtract;
-
+const fs = require('fs');
 puppeteer.use(StealthPlugin());
 
-const URL = 'https://www.uvic.ca/ecs/software/current-students/courses/index.php';
 
 const main = async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     const pages = await browser.pages();
+    const url = 'https://www.uvic.ca/ecs/software/current-students/courses/index.php';
     if (pages.length > 1) { await pages[0].close(); }
-    await page.goto(URL);
+    await page.goto(url);
 
-    const tableData: string[] = await page.$$eval('tbody', tbodies => {
+    const tableData: string[] = await page.$$eval('tbody', (tbodies: HTMLTableSectionElement[]) => {
         const data: string[] = [];
 
-        tbodies.forEach(tbody => {
-            const coldatA = Array.from(tbody.querySelectorAll('tr')).map(row => `[${row.children[0]?.textContent?.trim().replace(/\s+/g, ' ')}]`).filter(text => text).join('\n');
-            data.push(coldatA);
-
-            const coldatB = Array.from(tbody.querySelectorAll('tr')).map(row => `[${row.children[1]?.textContent?.trim().replace(/\s+/g, ' ')}]`).filter(text => text).join('\n');
-            data.push(coldatB);
+        tbodies.slice(0, 4).forEach(tbody => {
+            data.push(
+                Array.from(
+                    tbody.querySelectorAll('tr'))
+                    .map(row =>
+                        `[${row.children[0]
+                            ?.textContent
+                            ?.trim()
+                            .replace(/\s+/g, ' ')}]`)
+                    .filter(text => text)
+                    .join('\n'));
+            data.push(
+                Array.from(
+                    tbody.querySelectorAll('tr'))
+                    .map(row =>
+                        `[${row.children[1]
+                            ?.textContent
+                            ?.trim()
+                            .replace(/\s+/g, ' ')}]`)
+                    .filter(text => text)
+                    .join('\n'));
         });
 
         return data;
     });
 
+    let { technical = 0, natural = 0, complementary = 0 } = {};
     const jsonData = JSON.stringify(
         tableData.map((data) => {
-            const termAndSession = data.split('\n')[0].slice(1, -1);
+            const termAndSession = data.split('\n')[0].slice(6, -1);
             const [term, session] = termAndSession.split(' - ');
-            const courses = data.split('\n').slice(1).map(course => course.slice(1, -1));
+
+            const courses = data.split('\n').slice(1).map(course => {
+                if (course.includes(" OR ")) {
+                    const [option1, option2] = course.split(" OR");
+                    return {
+                        [option1.split(" - ")[1].slice(0, -2).trim()]: option1.split("-")[0].slice(1, -1).trim(),
+                        [option2.split(" - ")[1].slice(0, -2).trim()]: option2.split("-")[0].trim()
+                    };
+                } else {
+                    const [courseName, courseCode] = course.slice(1, -1).split("- ");
+                    if (courseCode === undefined && courseName.includes("Elective")) {
+                        // console.log(courseName);
+                        if (courseName.includes("Technical")) {
+                            return { [courseName.trim()]: ++technical };
+                        } else if (courseName.includes("Natural")) {
+                            return {
+                                [courseName.trim()]: ++natural
+                            };
+                        } else if (courseName.includes("Complementary")) {
+                            return { [courseName.trim()]: ++complementary };
+                        }
+                    } else {
+                        return { [courseCode]: courseName.trim() };
+                    }
+                }
+            });
+
+
             return {
                 term,
                 session,
@@ -44,7 +86,8 @@ const main = async () => {
     );
 
     console.log(jsonData);
-
+    // fs.writeFileSync('./data.json', jsonData, 'utf8');
+    // console.log('JSON data has been saved to data.json');
     await browser.close();
 };
 
