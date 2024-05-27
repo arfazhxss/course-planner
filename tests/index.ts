025dbde1d18ad144f56826d499e286d99d3c78e5
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-05-27 02:59:19 -0700

    current scrapped seng course-data

--- tests/index.ts
+++ tests/index.ts
@@ -42,37 +42,74 @@ const main = async () => {
         return data;
     });
 
-    let { technical = 0, natural = 0, complementary = 0 } = {};
+    let { technical = 0, natural = 0, complementary = 0, unit = 1.5 } = {};
     const jsonData = JSON.stringify(
         tableData.map((data) => {
             const termAndSession = data.split('\n')[0].slice(6, -1);
             const [term, session] = termAndSession.split(' - ');
 
             const courses = data.split('\n').slice(1).map(course => {
+                const [courseCode, courseName] = course.slice(1, -1).split("- ");
                 if (course.includes(" OR ")) {
                     const [option1, option2] = course.split(" OR");
-                    return {
-                        [option1.split(" - ")[1].slice(0, -2).trim()]: option1.split("-")[0].slice(1, -1).trim(),
-                        [option2.split(" - ")[1].slice(0, -1).replace(/[^a-zA-Z\s]/g, '').trim()]: option2.split("-")[0].trim()
-                    };
+                    return [
+                        {
+                            courseCode: option1.split("-")[0].slice(1, -1).trim(),
+                            courseName: option1.split(" - ")[1].slice(0, -2).trim(),
+                            courseType: "Mandatory",
+                            unit: unit
+                        },
+                        {
+                            courseCode: option2.split("-")[0].trim(),
+                            courseName: option2.split(" - ")[1].slice(0, -1).replace(/[^a-zA-Z\s]/g, '').trim(),
+                            courseType: "Mandatory",
+                            unit: unit
+                        }
+                    ];
+
                 } else {
-                    const [courseName, courseCode] = course.slice(1, -1).split("- ");
-                    if (courseCode === undefined && courseName.includes("Elective")) {
-                        // console.log(courseName);
-                        if (courseName.includes("Technical")) {
-                            return { [courseName.trim()]: ++technical };
-                        } else if (courseName.includes("Natural")) {
+                    if ((courseName === undefined) && (courseCode === "")) {
+                        // return {
+                        //     courseCode: "",
+                        //     courseType: "",
+                        //     courseName: "",
+                        //     unit: ""
+                        // };
+                        return;
+                    }
+                    else if (courseName === undefined && courseCode.includes("Elective")) {
+                        if (courseCode.includes("Technical")) {
                             return {
-                                [courseName.trim()]: ++natural
+                                courseCode: "",
+                                courseType: "Elective",
+                                courseName: `${courseCode.slice(0, -8).trim()} #${++technical}`,
+                                unit: unit
                             };
-                        } else if (courseName.includes("Complementary")) {
-                            return { [courseName.trim()]: ++complementary };
+                        } else if (courseCode.includes("Natural")) {
+                            return {
+                                courseCode: "",
+                                courseType: "Elective",
+                                courseName: `${courseCode.slice(0, -9).trim()} #${++natural}`,
+                                unit: unit
+                            };
+                        } else if (courseCode.includes("Complementary")) {
+                            return {
+                                courseCode: "",
+                                courseType: "Elective",
+                                courseName: `${courseCode.slice(0, -8).trim()} #${++complementary}`,
+                                unit: unit
+                            };
                         }
                     } else {
-                        return { [courseCode]: courseName.trim() };
+                        return {
+                            courseCode: courseCode.trim(),
+                            courseName: courseName,
+                            courseType: "Mandatory",
+                            unit: unit
+                        };
                     }
                 }
-            });
+            }).filter(course => course !== undefined);;
 
 
             return {
@@ -86,8 +123,8 @@ const main = async () => {
     );
 
     console.log(jsonData);
-    // fs.writeFileSync('./data.json', jsonData, 'utf8');
-    // fs.writeFileSync('../course-data.json', jsonData, 'utf8');
+    fs.writeFileSync('./data.json', jsonData, 'utf8');
+    fs.writeFileSync('../course-data.json', jsonData, 'utf8');
     console.log('JSON data has been saved to data.json');
     await browser.close();
 };
